a527475aa42675f869fe2c51c4c4571ab3e5c2be
yu_liukun <yu_liukun@outlook.com>
2022-12-03 11:59:41 +0800

    add `pow`

--- mat_calc/src/prelude.rs
+++ mat_calc/src/prelude.rs
@@ -1,29 +1,32 @@
-use crate::eval::{ObjectPairItem, EvalError, Frame, BuiltinFunction};
 use crate::eval::Environment;
+use crate::eval::{BuiltinFunction, EvalError, Frame, ObjectPairItem};
 
-use std::rc::Rc;
 use indoc::indoc;
+use std::rc::Rc;
 
 type Output = Result<ObjectPairItem, EvalError>;
 
-mod numeric;
-mod matrix;
-mod list;
 mod constants;
-
+mod list;
+mod matrix;
+mod numeric;
 
 /// Inject all builtin functions in to `frame`, which is the root frame
 pub fn inject_builtins(frame: &mut Frame) {
-    for (name, argn, f) in 
-        numeric::EXPORTS
-            .into_iter()
-            .chain(list::EXPORTS.into_iter())
-            .chain(matrix::EXPORTS.into_iter()) {
-        
+    for (name, argn, f) in numeric::EXPORTS
+        .into_iter()
+        .chain(list::EXPORTS.into_iter())
+        .chain(matrix::EXPORTS.into_iter())
+    {
         let builtin_func = BuiltinFunction {
-            f: Box::new(f), argn, name: name.to_string()
+            f: Box::new(f),
+            argn,
+            name: name.to_string(),
         };
-        frame.insert(name.to_string(), ObjectPairItem::BuiltinFunc(Rc::new(builtin_func)));
+        frame.insert(
+            name.to_string(),
+            ObjectPairItem::BuiltinFunc(Rc::new(builtin_func)),
+        );
     }
 
     for (val, name) in constants::EXPORTS.into_iter() {
@@ -49,15 +52,26 @@ pub const PRELUDE_SRC: &'static str = indoc! {"
         x
     )
 )
+
+# pow
+(def (pow x n)
+    (if (< n 2)
+        x
+        (* x (pow x (- n 1)))
+    )
+)
 )"
 };
 
 pub fn get_prelude_src() -> String {
-    PRELUDE_SRC.lines().filter(|line| !line.starts_with('#')).collect()
+    PRELUDE_SRC
+        .lines()
+        .filter(|line| !line.starts_with('#'))
+        .collect()
 }
 
 type ExportType = (
-    &'static str,  // name
-    usize,  // arg count
+    &'static str, // name
+    usize,        // arg count
     &'static dyn Fn(ObjectPairItem, &mut Environment) -> Output,
 );
